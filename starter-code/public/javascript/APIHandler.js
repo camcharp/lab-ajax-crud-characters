8fed3c4227c855411f3fe046f1b78cabc723e0e5
Camille Charpentier <cam.charpentier@live.fr>
2019-06-24 18:34:15 +0200

    edit iteration not working

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,85 @@
+const productAPI = axios.create({
+	baseURL: 'http://localhost:8000'
+});
+
+const name = document.getElementById('name');
+const occupation = document.getElementById('occupation');
+const weapon = document.getElementById('weapon');
+const checkbox = document.getElementById('checkbox');
+
+const id = document.getElementById('editid');
+const editname = document.getElementById('editname');
+const editoccupation = document.getElementById('editoccupation');
+const editweapon = document.getElementById('editweapon');
+const editcheckbox = document.getElementById('editcheckbox');
+
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+	constructor(baseUrl) {
+		this.BASE_URL = baseUrl;
+	}
 
-  getFullList () {
+	getFullList() {
+		productAPI
+			.get('/characters')
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
 
-  }
+	getOneRegister(id) {
+		productAPI
+			.get(`/characters/${id}`)
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+	createOneRegister() {
+		productAPI
+			.post('/characters', {
+				name: name.value,
+				occupation: occupation.value,
+				weapon: weapon.value,
+				checkbox: checkbox.value
+			})
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
 
-  getOneRegister () {
+	updateOneRegister(id) {
+		productAPI
+			.patch(`/characters/${id}`, {
+				name: editname.value,
+				occupation: editoccupation.value,
+				weapon: editweapon.value,
+				checkbox: editcheckbox.value
+			})
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+	deleteOneRegister(id) {
+		productAPI
+			.delete(`/characters/${id}`)
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,26 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000');
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+	document.getElementById('fetch-all').onclick = function() {
+		charactersAPI.getFullList();
+	};
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+	const inputId = document.getElementById('input-id');
+	document.getElementById('fetch-one').onclick = function() {
+		charactersAPI.getOneRegister(inputId.value);
+	};
+
+	const deleteId = document.getElementById('delete-id');
+	document.getElementById('delete-one').onclick = function() {
+		charactersAPI.deleteOneRegister(deleteId.value);
+	};
+
+	const editId = document.getElementById('edit-character-form');
+	document.getElementById('edit-character-form').onsubmit = function() {
+		charactersAPI.updateOneRegister(editId.value);
+	};
+
+	document.getElementById('new-character-form').onsubmit = function() {
+		charactersAPI.createOneRegister();
+	};
+});
